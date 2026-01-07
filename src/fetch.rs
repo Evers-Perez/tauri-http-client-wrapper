0e697c1b3860aa0b08dabe1f1d90b73dbc315bd7
Evers Perez <evers.perez@ukg.com>
2026-01-07 15:26:36 -0500

    chore: remove unused fetch module

--- src/fetch.rs
+++
@@ -1,7 +0,0 @@
-use tauri_plugin_http::reqwest;
-
-fn fetch(url){
-let res = reqwest::get("http://my.api.host/data.json").await;
-println!("{:?}", res.status()); // e.g. 200
-println!("{:?}", res.text().await); // e.g Ok("{ Content }")
-})
