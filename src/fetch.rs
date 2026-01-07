3a379fa565e5e8572d02f80dafce84ceb1d2cc5e
Evers <epere439@fiu.edu>
2026-01-06 21:30:08 -0500

    wip: add fetch mod

---
+++ src/fetch.rs
@@ -0,0 +1,7 @@
+use tauri_plugin_http::reqwest;
+
+fn fetch(url){
+let res = reqwest::get("http://my.api.host/data.json").await;
+println!("{:?}", res.status()); // e.g. 200
+println!("{:?}", res.text().await); // e.g Ok("{ Content }")
+})
